53ea37fb495e4322e03da0d7e276ba6036313530
XP20 <toms.b.gm@gmail.com>
2023-12-03 22:41:21 +0200

    progress obj_parse

--- src/obj_parse.rs
+++ src/obj_parse.rs
@@ -1,6 +1,7 @@
 pub mod obj {
-    pub fn parse(content: &str) -> Vec<f32> {
-        let mut res: Vec<f32> = Vec::new();
+    pub fn parse(content: &str) -> (Vec<f32>, Vec<u16>) {
+        let mut verts: Vec<f32> = Vec::new();
+        let mut indices: Vec<u16> = Vec::new();
 
         let lines = content.lines();
         for line in lines {
@@ -9,11 +10,28 @@ pub mod obj {
                 continue;
             };
             match identifier {
-                "v" => continue,
+                "v" => {
+                    let x = iter.next().unwrap_or("0.0").parse::<f32>().unwrap();
+                    let y = iter.next().unwrap_or("0.0").parse::<f32>().unwrap();
+                    let z = iter.next().unwrap_or("0.0").parse::<f32>().unwrap();
+                    let _w = iter.next().unwrap_or("1.0").parse::<f32>().unwrap();
+                    verts.extend_from_slice(&[x, y, z]);
+                },
+                "f" => {
+                    let x = iter.next().unwrap().split('/').next().unwrap().parse::<u16>().unwrap();
+                    let y = iter.next().unwrap().split('/').next().unwrap().parse::<u16>().unwrap();
+                    let z = iter.next().unwrap().split('/').next().unwrap().parse::<u16>().unwrap();
+                    if let Some(item) = iter.next() {
+                        let w = item.split('/').next().unwrap().parse::<u16>().unwrap();
+                        indices.extend_from_slice(&[x, y, z, x, z, w]);
+                    } else {
+                        indices.extend_from_slice(&[x, y, z]);
+                    }
+                },
                 _ => continue,
             }
         }
 
-        return res;
+        return (verts, indices);
     }
 }
